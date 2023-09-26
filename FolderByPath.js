b21f85ff0fa7b910c2d87b3f2221dc0812654aed
Primbrook <68938110+Primbrook@users.noreply.github.com>
2023-09-26 08:56:01 -0500

    More Indenting

--- FolderByPath.js
+++ FolderByPath.js
@@ -16,18 +16,18 @@ function folderFinder(path, altRoot, mkdir) {
     var errmsg = "Alternet root folder ID is invalid.";
     var bPath = DriveApp.getRootFolder;
     if (!altRoot){
-     Logger.log("No base path provided! Defaulting to users drive");
-     var bPath = DriveApp.getRootFolder().getId();
-     Logger.log("Default Path = "+bPath);
+      Logger.log("No base path provided! Defaulting to users drive");
+      var bPath = DriveApp.getRootFolder().getId();
+      Logger.log("Default Path = "+bPath);
     }else if (altRoot != null){
      try {
-       DriveApp.getFolderById(altRoot);
+        DriveApp.getFolderById(altRoot);
      } catch {
-       Logger.log("Invalid root folder ID provided. Aborting."); return errmsg
+          Logger.log("Invalid root folder ID provided. Aborting."); return errmsg
      }
-     Logger.log("Alternate root ID is valid");
-     bPath = altRoot;
-     Logger.log("Alternate Path = "+bPath);
+            Logger.log("Alternate root ID is valid");
+            bPath = altRoot;
+            Logger.log("Alternate Path = "+bPath);
     }
     //BASE PATH CHECKER END
    
@@ -37,18 +37,18 @@ function folderFinder(path, altRoot, mkdir) {
     for (var x = 0; x < path.length; x++) {
      Logger.log((x+1) + " checking for folder named "+path[x]);
      if (DriveApp.getFolderById(bPath).getFoldersByName(path[x]).hasNext() === true){
-       Logger.log("Folder '"+path[x]+"' exists!");
-       Logger.log(DriveApp.getFolderById(bPath).getFoldersByName(path[x]).next().getId());
-       var nPath = DriveApp.getFolderById(bPath).getFoldersByName(path[x]).next().getId();
-       bPath = nPath;
+        Logger.log("Folder '"+path[x]+"' exists!");
+        Logger.log(DriveApp.getFolderById(bPath).getFoldersByName(path[x]).next().getId());
+        var nPath = DriveApp.getFolderById(bPath).getFoldersByName(path[x]).next().getId();
+        bPath = nPath;
      } else if (mkdir === true) {
-       Logger.log(path[x]+ " not found. Mkdir set to true, making folder");
-       var nPath = DriveApp.getFolderById(bPath).createFolder(path[x]).getId();
-       bPath = nPath;
+          Logger.log(path[x]+ " not found. Mkdir set to true, making folder");
+          var nPath = DriveApp.getFolderById(bPath).createFolder(path[x]).getId();
+          bPath = nPath;
      } else {
-       Logger.log("Could not find folder '"+path[x]+"' and mkdir is either set to false or is unset")
-       var errmsg = "Provided path did not work";
-       return errmsg;
+            Logger.log("Could not find folder '"+path[x]+"' and mkdir is either set to false or is unset")
+            var errmsg = "Provided path did not work";
+            return errmsg;
      }
     }
     //FOLDER FINDER END
